2120d2b8c50aa33415a0629b353414bfdf11cd20
fullstackdeveloper918 <fullstackdeveloper918@gmail.com>
2024-01-18 13:30:15 +0530

    topbar with responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,24 @@ import DashBoard from "@/components/DashBoard";
 import Card from "./../components/Card";
 import { useEffect, useState } from "react";
 import AdditionalServicesCard from "./../components/AdditionalServicesCard/AdditionalServicesCard";
+import PrimaryHeader from "./../components/primary-header/PrimaryHeader";
 
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="">
+        <PrimaryHeader />
+      </div>
+      {/* <div>
         <AdditionalServicesCard />
       </div>
       <main>
         <Card />
         <section className="min-h-[100vh] w-[100%]">
-          {/* <section>hellokjmh</section> */}
           <DashBoard />
           <div className="flex items-center justify-between"></div>
         </section>
-      </main>
+      </main> */}
     </>
   );
 }
---
+++ components/primary-header/PrimaryHeader.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./index.css";
+import Image from "next/image";
+import downArrowImg from "../../assets/headerPics/down.svg";
+import phoneImg from "../../assets/headerPics/phone.svg";
+import searchImg from "../../assets/headerPics/Search.svg";
+import { type } from "./../../redux-setup/store";
+const PrimaryHeader = () => {
+  return (
+    <div className="main_container">
+      <div className="container">
+        <div className="container_left">
+          <div className="offer_container">
+            <div className="">
+              <Image src={phoneImg} alt="down arrow" width={16} height={15} />
+            </div>
+            <div className="">1-877-256-6998</div>
+          </div>
+          <div className="offer_container">
+            <div className="">What we Offer</div>
+            <div className="">
+              <Image src={downArrowImg} alt="down arrow" width={9} height={9} />
+            </div>
+          </div>
+        </div>
+        <div className="container_right">
+          <div className="search_container">
+            <Image src={searchImg} alt="down arrow" width={12} height={12} />
+
+            <input type="text" placeholder="Search for product..." />
+          </div>
+          <div className="">Login</div>
+          <div className="">SignUp</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrimaryHeader;
