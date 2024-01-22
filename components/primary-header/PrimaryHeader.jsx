862bd0cb61773dece28a5753d19b2aa4fe3847d1
fullstackdeveloper918 <fullstackdeveloper918@gmail.com>
2024-01-22 19:15:41 +0530

    ani_code

--- components/primary-header/PrimaryHeader.jsx
+++ components/primary-header/PrimaryHeader.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import "./index.css";
 import Image from "next/image";
 import downArrowImg from "../../assets/headerPics/down.svg";
 import phoneImg from "../../assets/headerPics/phone.svg";
 import searchImg from "../../assets/headerPics/Search.svg";
 import { type } from "./../../redux-setup/store";
+import loginIcon from "../../assets/login-icon.svg"
+import signIcon from "../../assets/headerPics/Sign-up.svg"
 const PrimaryHeader = () => {
+  const [screenSize, setScreenSize] = useState(760)
   return (
     <div className="main_container top-bar">
       <div className="container">
@@ -25,12 +29,32 @@ const PrimaryHeader = () => {
         </div>
         <div className="container_right">
           <div className="search_container">
-            <Image src={searchImg} alt="down arrow" width={12} height={12} />
+            <Image src={searchImg} alt="down arrow" width={16} height={16} />
 
             <input type="text" placeholder="Search for product..." />
           </div>
-          <div className="">Login</div>
-          <div className="">SignUp</div>
+          <div className="login-signup">
+          {screenSize > 767 && (
+            'Login'
+            )}
+          {screenSize < 767 && (
+            <span>
+              <Image src={loginIcon} alt="login" width={13} height={13} />
+            </span>
+            )}
+          </div>
+          
+          
+          <div className="login-signup">
+          {screenSize > 767 && (
+            'SignUp'
+            )}
+          {screenSize < 767 && (
+            <span>
+              <Image src={signIcon} alt="login" width={18} height={18} />
+            </span>
+            )}
+          </div>
         </div>
       </div>
     </div>
