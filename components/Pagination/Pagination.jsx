ae6f547b50012f12b6b6b1c9da234f470d3b9c3c
Kashyap@9180 <thakur.ashok9180@gmail.com>
2024-01-25 16:27:55 +0530

    pagination ui

--- components/Pagination/Pagination.jsx
+++ components/Pagination/Pagination.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "./index.css";
+import "./Pagination.css";
 const Pagination = ({ totalCount = 50, limit = 10 }) => {
   let pages = totalCount / limit;
   const pageArray = Array.from({ length: pages }, (_, index) => index + 1);
   return (
     <>
-      <div class="pagination">
+      <div class="pagination_container">
         <button>Previous</button>
         {pageArray?.map((p) => {
           return <button>{p}</button>;
